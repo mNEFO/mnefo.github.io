818008364b3cde2d03a76d2e2bcf95de35a17aa2
mNEFO <hpmicroserver@gmail.com>
2026-09-18 22:40:09 +0900

    update

--- web_serial/app.js
+++ web_serial/app.js
@@ -426,10 +426,10 @@ function parseReceivedJson(jsonString) {
                     valBoardId.textContent = data.board_id;
                 }
                 if (data.flash_usage !== 0) {
-                    valStorage.textContent = data.flash_usage;
+                    valStorage.textContent = `${data.flash_usage} %`;
                 }
                 if (data.flash_usage === 0) {
-                    valStorage.textContent = 0;
+                    valStorage.textContent = "0 %";
                 }
                 if (data.pico_temp) {
                     valBoardTemp.textContent = data.pico_temp.toFixed(1);
